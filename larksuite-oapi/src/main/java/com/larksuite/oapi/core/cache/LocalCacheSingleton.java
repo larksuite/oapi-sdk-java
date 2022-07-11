f505ad844520a5e1f82d9adee0c93e6eb19f7b3a
Chenlong <88413945@qq.com>
2022-07-11 20:50:33 +0800

    Code reviews, add TODO

--- larksuite-oapi/src/main/java/com/larksuite/oapi/core/cache/LocalCacheSingleton.java
+++ larksuite-oapi/src/main/java/com/larksuite/oapi/core/cache/LocalCacheSingleton.java
@@ -1,5 +1,6 @@
 package com.larksuite.oapi.core.cache;
 
+// TODO 直接由 LocalCache 提供单例是不是更简洁
 public class LocalCacheSingleton {
     private static final LocalCache LOCAL_CACHE = new LocalCache();
 
--- larksuite-oapi/src/main/java/com/larksuite/oapi/core/exception/NeedAccessTokenException.java
+++ larksuite-oapi/src/main/java/com/larksuite/oapi/core/exception/NeedAccessTokenException.java
@@ -1,5 +1,6 @@
 package com.larksuite.oapi.core.exception;
 
+// TODO 这个异常命名可读性不太好
 public class NeedAccessTokenException extends RuntimeException {
     public NeedAccessTokenException() {
         super("Need pass an access token value");
