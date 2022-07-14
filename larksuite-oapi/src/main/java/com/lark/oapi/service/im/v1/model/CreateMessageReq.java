e928ab01b582f7c10a42ed80ac976192b8af7f27
陈龙 <chenlong.work@bytedance.com>
2022-07-14 11:06:38 +0800

    add todo commit

--- larksuite-oapi/src/main/java/com/lark/oapi/service/im/v1/model/CreateMessageReq.java
+++ larksuite-oapi/src/main/java/com/lark/oapi/service/im/v1/model/CreateMessageReq.java
@@ -47,6 +47,7 @@ public class CreateMessageReq {
     private String receiveIdType;
     private CreateMessageReqBody body;
 
+    // todo 参数使用枚举更好些。生成代码时有困难？
     public Builder receiveIdType(String receiveIdType) {
       this.receiveIdType = receiveIdType;
       return this;
